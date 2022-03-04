b2b6fe4e81fe31fc9ec4efaefd564894d8ca7224
Jonathan Florido-Carait <jonathan.carait@outlook.com>
2022-03-04 21:25:11 +1100

    deleted a few files no longer needed

--- utils/helper.js
+++
@@ -1,7 +0,0 @@
-// import config connection to sql database
-const db = require("../config/connection")
-
-// Helper function for display SQL data into console table
-
-
-module.exports = displayTable;
